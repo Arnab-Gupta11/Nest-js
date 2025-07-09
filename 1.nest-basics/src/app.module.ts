cc2bdac6c1d4c00123d78722dac33962c9d61fb1
Arnab Gupta <arnabgupta1111@gmail.com>
2025-07-10 03:25:51 +0600

    1.nest-basics: add tweets and users controller and module

--- 1.nest-basics/src/app.module.ts
+++ 1.nest-basics/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './users/users.module';
+import { TweetsModule } from './tweets/tweets.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, TweetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
