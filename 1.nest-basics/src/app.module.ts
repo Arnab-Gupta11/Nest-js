2e5c87b2df54374af409d2ba2b5a6a712a023118
Arnab Gupta <arnabgupta1111@gmail.com>
2025-07-14 04:30:57 +0600

    1.nest-basics: connect postegres database with typeORM.

--- 1.nest-basics/src/app.module.ts
+++ 1.nest-basics/src/app.module.ts
@@ -2,14 +2,45 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
-
 import { TweetModule } from './tweet/tweet.module';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, TweetModule, AuthModule],
+  imports: [
+    UserModule,
+    TweetModule,
+    AuthModule,
+
+    // ====> Asynchronus connection <====
+
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        entities: [],
+        synchronize: true,
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: '12345',
+        database: 'nestjs',
+      }),
+    }),
+
+    // ====> synchronus connection <====
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   entities: [],
+    //   synchronize: true,
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: '12345',
+    //   database: 'nestjs',
+    // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
