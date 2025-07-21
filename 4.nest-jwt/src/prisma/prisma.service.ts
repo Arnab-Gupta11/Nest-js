69ed8af083e9e71d6750da8e6f9ac591c45e472c
Arnab Gupta <arnabgupta1111@gmail.com>
2025-07-22 01:10:16 +0600

    4.nest-jwt: Initialize project and connect to db

---
+++ 4.nest-jwt/src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://postgres:123456@localhost:5432/nest_jwt_db?schema=public',
+        },
+      },
+    });
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
